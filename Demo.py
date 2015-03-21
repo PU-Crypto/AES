8febb3958dbebcca5cd9656003cb9369051e3236
Tamwyn <games_gamer@ymail.com>
2015-03-21 13:55:57 +0100

    Klasse RijndaelRconField in RijndaelRcon umbenannt um das einbinden etwas übersichtlicher zu gestalten

--- Demo.py
+++ Demo.py
@@ -1,8 +1,8 @@
 #So werden die verschiedenen Tabellen beispielhaft eingebunden:
 from Tables import RijndaelSBox
-from Tables import RijndaelRconField
+from Tables import RijndaelRcon
 
 output = RijndaelSBox.SBox[2]
 print(output[0])
 
-print(RijndaelRconField.Rcon[1])
+print(RijndaelRcon.Rcon[1])
