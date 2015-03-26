aad6ce669c75e5cdb8acedc0b988281f1c95cb98
KlausGerhart <klaus.gerhart@web.de>
2015-03-26 14:29:41 +0100

    fehler behoben, funktion draus gemacht

--- shiftrows.py
+++ shiftrows.py
@@ -1,10 +1,11 @@
-Block=[]
-for i in range(0,4):
-	Zeile=Block[i]
-	#dump=Block[i]		#wozu ist dieser schritt gut? rall ich grade nicht.
-	for n in range(0,n):
-		dump[0] = Zeile[1]
-		dump[1] = Zeile[2]
-		dump[2] = Zeile[3]
-		dump[3] = Zeile[0]
-	Block[i] = dump
+def shiftrows(Block):		# Block ist der einzulesene Array
+	Block=[]
+	for i in range(0,4):
+		Zeile=Block[i]
+		dump[]
+		for n in range(0,i):
+			dump[0] = Zeile[1]
+			dump[1] = Zeile[2]
+			dump[2] = Zeile[3]
+			dump[3] = Zeile[0]
+		Block[i] = dump
