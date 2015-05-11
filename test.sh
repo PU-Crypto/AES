e4aef159721605a20b1e98ab7ce119119d0ee618
Tamwyn <tamwyn.eopia@live.de>
2015-05-11 21:34:02 +0200

    Script um pwgen erweitert

--- test.sh
+++ test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-	echo "Enter a string with no '' or ' ' "
-	read string
-	
-	cipher=$(python Rijndael.py -m=e -p=Password#Very#Secure -t=$string)
+COUNTER=0
+while [  $COUNTER -lt 10 ]; do
+#	echo "Enter a string with no '' or ' ' "
+#	read string
+	string=$(pwgen -N 1 -s -y 12)
+	echo "$string"	
+	cipher=$(python3 Rijndael.py -m=e -p=Password#Very#Secure -t=$string)
 
 #	echo "$cipher"
 	
 	
-	text=$(python Rijndael.py -m=d -p=Password#Very#Secure -t=$cipher)
+	text=$(python3 Rijndael.py -m=d -p=Password#Very#Secure -t=$cipher)
 
 #	echo "$text"
 	
@@ -21,6 +24,8 @@
 		echo "Everything was done right! Have some nice sleep"
 	fi
 
-
+	let COUNTER=COUNTER+1
+done
+						    
 
 
