682e9d20f901e4427f9241a6a9695c141799b54e
Tamwyn <tamwyn.eopia@live.de>
2015-05-13 22:40:07 +0200

    Aufräumarbeiten
    Da das Debugging nach Tests abgeschlossen werden konnte, wurden die
    Scripte in einen Branch verschoben und eine GUI kompatible Version
    erstellt

--- Rijndael.py
+++ Rijndael.py
@@ -46,27 +46,24 @@ def RijndaelRechnung(text, key): #text ist ein 4x4 Block(zeilenorientiert) mit H
 
 def Rijndael(text,password):
 	key = KeyGen.KeyGen(password) #generiere aus dem Passwort den Key
-	preciphertext = list()
 	key = KeySchedule(key) #Erweitere den Schluessel
 	plain = UTF8.UTFConvert(text)
-	cipher = CBC.CBC_Encrypt(plain, '10101011')
-	RijndaelBlock = CBC.GenRijndaelBlock(cipher) #Forme Listen mit 4 Hexadezimalwerten
+	RijndaelBlock = CBC.GenRijndaelBlock(plain) # Wende CBC an und forme Listen mit 4 Hexadezimalwerten
 	
 	Block4x4 = list() #Forme 4x4 Bloecke und uebergib sie an Rijndael
+	preciphertext = list()
 	for i in range(0,len(RijndaelBlock)):
 		Block4x4.append(RijndaelBlock[i])
 		if len(Block4x4) == 4:
-			for j in range(0,4): #formt einen Xx4x4 Array in einen Xx4 Array
-				a = RijndaelRechnung(Block4x4, key)
-				preciphertext.append(a[j]) #Uebergib den 4x4 Block an Rijndael
-			
+			encrypted = RijndaelRechnung(Block4x4, key) #Uebergib den 4x4 Block an Rijndael
+			for Zeile in encrypted:
+				preciphertext.append(Zeile)
 			Block4x4 = list()
 	ciphertext = ''
 	for Zeile in preciphertext:
 		for Wert in Zeile:
 			Wert = Wert.split('x')[1]
-			ciphertext += Wert
-		
+			ciphertext += Wert	
 	return ciphertext
 
 def RijndaelDecryptRechnung(cipher, key): #cipher ist ein 4x4 Block(zeilenorientiert) mit Hexadezimalwerten im Format 0x.. ; key ist der bereits erweiterte Schluessel
@@ -78,7 +75,7 @@ def RijndaelDecryptRechnung(cipher, key): #cipher ist ein 4x4 Block(zeilenorient
 	text = DeShiftRows(text)
 	text = InvSubBytes(text)
 	
-	#Ausfuehrung der 9 vollstaendigen Runden andersherum als bei der Verschluesellung. Die range kommt durch GetCurrentKey zustande, ebenfalls invers zur verschluesselung
+	#Ausfuehrung der 9 vollstaendigen Runden andersherum als bei der Verschluessellung. Die range kommt durch GetCurrentKey zustande, ebenfalls invers zur Verschluesselung
 	for i in range(36, 3, -4):
 		currentKey = GetCurrentKey(i, key)
 		text = AddRoundKey(currentKey, text)
@@ -94,10 +91,9 @@ def RijndaelDecryptRechnung(cipher, key): #cipher ist ein 4x4 Block(zeilenorient
 
 
 
-def RijndaelDecrypt(cipher,password): #Entschluessel mit Passwort und dem dem Cipher
+def RijndaelDecrypt(cipher,password): #Entschluessel mit dem Passwort und dem Cipher
 	precipher = list(cipher) #Schreibt Cipher in eine Liste
 	HexArray = list()
-		
 	for i in range (0,len(precipher),2): #Macht die einzelnen Eintraege zu 0xAB - Eintraegen
 		paar = '0x' + precipher[i] + precipher[i+1]
 		HexArray.append(paar)
@@ -108,6 +104,7 @@ def RijndaelDecrypt(cipher,password): #Entschluessel mit Passwort und dem dem Ci
 		if len(dump) == 4:
 			cipher.append(dump)
 			dump = list()
+	
 	key = KeyGen.KeyGen(password) #generiert den Key aus dem Passwort
 	key = KeySchedule(key) #erweitert den Key
 	
@@ -116,23 +113,23 @@ def RijndaelDecrypt(cipher,password): #Entschluessel mit Passwort und dem dem Ci
 	for i in range(0,len(cipher)): #macht 4x4 Bloecke
 		Block4x4.append(cipher[i])
 		if len(Block4x4) == 4:
-	
 			decrypted.append(RijndaelDecryptRechnung(Block4x4, key)) #Uebergib den 4x4 Block an Rijndael
-			
 			Block4x4 = list()
 	
-	#Entferne die Bloecke und forme ein Liste aus einzelnen Werten und entferne reine 0 Bloecke
-	
+	#Entferne die Bloecke und forme ein Liste aus einzelnen Werten
 	decryptedList = list()
 	for Block in decrypted:
 		for Zeile in Block:
 			for Wert in Zeile:
-				if Wert != '0x00':
-					decryptedList.append(Wert)
-	
-	plain = CBC.CBC_Decrypt(decryptedList)#Mache CBC rueckgaengig
-	plain=UTF8.UTFdeConvert(plain) #Erstelle aus den Zahlen nach der UTF-Tabelle Buchstaben
-	return plain#Gib das entschluesselte Ergebnis aus
+				decryptedList.append(Wert)
+	#decryptedList = CBC.CBC_Decrypt(decryptedList)#Mache CBC rueckgaengig
+	plain = list()
+	for wert in decryptedList:
+		wert = "{0:b}".format(int(wert,16))
+		wert = UTF8.Padding(wert,8) #Stelle das alte 8-stellige Format wieder her
+		plain.append(wert)
+	text=UTF8.UTFdeConvert(plain) #Erstelle aus den Zahlen nach der UTF-Tabelle Buchstaben
+	return text#Gib das entschluesselte Ergebnis aus
 
 def handleShellParam(param, default):
 
@@ -146,10 +143,6 @@ def handleShellParam(param, default):
 		elif(("-" + param) in cmdarg):
 			return str(cmdarg.replace(("-"), ""))
 	return default
-mode = handleShellParam("m", 0) 
-#Zur Bestimmung was gerade von dem Script verlangt wird, sonst exited er, wenn man verschluesseln will und natuerlich das Password fehlt fuer Encrypt die 1 und fuer Decrypt die 2
-password = handleShellParam("p", 0)
-text = handleShellParam("t", 0)
 
 def checkResult(textIn, encrypted, password): #Nimm den Eingangstext und den Verschlüsselten Text und versuche beide zu vergleichen
 	try:
@@ -157,39 +150,49 @@ def checkResult(textIn, encrypted, password): #Nimm den Eingangstext und den Ver
 		if decrypted == textIn:
 			return True
 		else:
+			#e = sys.exc_info()[0]
+			#print("Error in run: \n" + str(e))
 			return False
 	except:
+		e = sys.exc_info()[0]
+		print("Error in run: \n" + str(e))
 		return False
 
+mode = handleShellParam("m", 0) 
+#Zur Bestimmung was gerade von dem Script verlangt wird, sonst exited er, wenn man verschluesseln will und natuerlich das Password fehlt fuer Encrypt die 1 und fuer Decrypt die 2
+password = handleShellParam("p", 0)
+text = handleShellParam("t", 0)
 
+#mode='e'
+#password='StrengstensGeheim'
+#if mode=='e':
+#	text='eivaejooheepiyahshogahqu'
+#else:
+#	text='230e564c7ff6099750826e0a1b97e8c729de59d7927b3ce13d67441a2bf051f2'
 
 if mode == 'e':
 	if password == 0 or text == 0:
 		print('Bitte fuellen Sie sowohl Password als auch Entschluesselungsfeld aus')
 		sys.exit(1)
+	
 	if password != 0 and text != 0:
 		password += 'saltibus#Minnimax' #Salte das Passwort
 
 		loopprevent=0
-		erfolg = False
-		while(erfolg != True or loopprevent < 4): #Teste ob die Verschluesselung funktioniert hat sollte loopprevent ausschlagen gib eine Fehlermeldung aus
+		while(loopprevent < 3): #Teste ob die Verschluesselung funktioniert hat sollte loopprevent ausschlagen gib eine Fehlermeldung aus
 			encrypted = Rijndael(text, password)
 			if checkResult(text,encrypted,password):
-				erfolg = True
 				print(encrypted)
 				break;
-			erfolg = False
 			loopprevent += 1
-			if loopprevent == 4:
-				e = sys.exc_info()[0]
-				print("Dieser Text konnte nicht verschluesselt werden \n" + str(e))
-				sys.exit(1)
+			print()
+		if loopprevent == 4:
+			print("Dieser Text konnte nicht verschluesselt werden")
+			sys.exit(1)
 		sys.exit(0)
 
 if mode == 'd':
 	if password!=0 and text != 0:
-	#	if len(text)%2 != 0:
-	#		exit("Ist es möglich, dass ein Zeichen verloren gegangen ist")
 		password += 'saltibus#Minnimax'
 		try:
 			decrypted = RijndaelDecrypt(text, password)
